192804397b0155745d079b7f34f3993414603cca
Bruno de Carvalho <48860506+brunoak@users.noreply.github.com>
2026-03-18 13:07:49 -0300

    Fix exception handling for requests in modelos.py

--- modelos.py
+++ modelos.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from requests.exceptions import RequestException
 import requests
 
 class AtivoFinanceiro(ABC):
@@ -17,26 +16,21 @@ class Dolar(AtivoFinanceiro):
     def buscar_cotacao_online(self):
         try:
             url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-            }
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
             response = requests.get(url, headers=headers)
             
             if response.status_code == 200:
                 data = response.json()
                 return float(data['USDBRL']['bid'])    
             return None
-        
-        except RequestException as req_err:
+        except requests.exceptions.RequestException as req_err:
             print(f"Erro na requisição: {req_err}")
             return None
             
     def buscar_dados_completos(self):
         try:
             url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-            }
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 data = response.json()
@@ -45,7 +39,7 @@ class Dolar(AtivoFinanceiro):
                     "variacao": float(data['USDBRL']['pctChange']) 
                 }
             return None
-        except RequestException as req_err:
+        except requests.exceptions.RequestException as req_err:
             print(f"Erro na requisição: {req_err}")
             return None
     
@@ -56,26 +50,21 @@ class Bitcoin(AtivoFinanceiro):
     def buscar_cotacao_online(self):
         try:
             url = 'https://economia.awesomeapi.com.br/last/BTC-USD'
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-            }
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
             response = requests.get(url, headers=headers)
             
             if response.status_code == 200:
                 data = response.json()
                 return float(data['BTCUSD']['bid'])
             return None
-        
-        except RequestException as req_err:
+        except requests.exceptions.RequestException as req_err:
             print(f"Erro na requisição: {req_err}")
             return None
             
     def buscar_dados_completos(self):
         try:
             url = 'https://economia.awesomeapi.com.br/last/BTC-USD'
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-            }
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 data = response.json()
@@ -84,6 +73,6 @@ class Bitcoin(AtivoFinanceiro):
                     "variacao": float(data['BTCUSD']['pctChange']) 
                 }
             return None
-        except RequestException as req_err:
+        except requests.exceptions.RequestException as req_err:
             print(f"Erro na requisição: {req_err}")
             return None
