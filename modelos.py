c1c9d5d0c7998e397445f8cdca1da1259c39c5f6
Bruno de Carvalho <48860506+brunoak@users.noreply.github.com>
2026-03-18 12:57:41 -0300

    Enhance buscar_cotacao_online with headers and comments
    Updated the buscar_cotacao_online method to include headers for the requests to mimic a browser. Adjusted URLs and comments for clarity regarding currency conversion.

--- modelos.py
+++ modelos.py
@@ -14,14 +14,21 @@ class Dolar(AtivoFinanceiro):
     def __init__(self, nome ="Dólar Americano"):
         super().__init__(nome)
         
-    def buscar_cotacao_online(self):
+   def buscar_cotacao_online(self):
         try:
-            url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
-            response = requests.get(url)
+            url = 'https://economia.awesomeapi.com.br/last/USD-BRL' # Mude para BTC-USD na do Bitcoin
+            
+            # 🎭 O DISFARCE: Fingindo ser o Google Chrome no Windows
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+            }
+            
+            # Enviamos o disfarce junto com o GET
+            response = requests.get(url, headers=headers)
             
             if response.status_code == 200:
                 data = response.json()
-                return float(data['USDBRL']['bid'])    
+                return float(data['USDBRL']['bid']) # Mude para BTCUSD na do Bitcoin
             return None
         
         except RequestException as req_err:
@@ -51,7 +58,13 @@ class Bitcoin(AtivoFinanceiro):
     def buscar_cotacao_online(self):
         try:
             url = 'https://economia.awesomeapi.com.br/last/BTC-USD'
-            response = requests.get(url)
+        
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+            }
+            
+            # Enviamos o disfarce junto com o GET
+            response = requests.get(url, headers=headers)
             
             if response.status_code == 200:
                 data = response.json()
